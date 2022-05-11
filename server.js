c5dac1235566e29c90e16103b9ceb0c70deed23a
Eric Spencer <spencer22eric@gmail.com>
2022-05-10 21:09:51 -0400

    fix sequelize addition

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
