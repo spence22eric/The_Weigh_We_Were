f4008934de4f1e7152e18e9ac16c33384a8a2798
Eric Spencer <spencer22eric@gmail.com>
2022-05-21 19:57:43 -0400

    add user post route and password hashing

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// get all users
 router.get('/', (req, res) => {
     User.findAll({
         // attributes: { exclude: ['password'] }
@@ -12,6 +13,7 @@ router.get('/', (req, res) => {
     })
 });
 
+// get a single user
 router.get('/:id', (req, res) => {
     User.findOne({
         where: {
@@ -25,6 +27,22 @@ router.get('/:id', (req, res) => {
         }
         res.json(dbUserData);
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// create a user
+router.post('/', (req, res) => {
+    User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
