561a9460b463c41f10480c074794183fa7af9ae3
Eric Spencer <spencer22eric@gmail.com>
2022-05-23 22:23:01 -0400

    add logout route and update redirects for login

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes');
-const selectionRoutes = require('./selection-routes');
+const homeRoutes = require('./home-routes.js');
+const selectionRoutes = require('./selection-routes.js');
 
 
+
+router.use('/selection', selectionRoutes);
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/selection', selectionRoutes);
+
 
 module.exports = router;
