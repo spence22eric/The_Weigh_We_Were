420488bcb821090e3b7b74c34e55b83dfb0f85a8
Eric Spencer <spencer22eric@gmail.com>
2022-05-23 20:21:46 -0400

    login handler progress and logout route added

---
+++ public/javascript/login.js
@@ -0,0 +1,6 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector().value.trim();
+    const password = document.querySelector().value.trim();
+}
